b0fe93a09acc739993f47f58c1b4af5682cabcbb
dafialmadinah <165459543+dafialmadinah@users.noreply.github.com>
2024-10-10 07:08:55 +0700

    login.php

--- login.php
+++ login.php
@@ -1,8 +1,35 @@
+<?php 
+
+include "service/user.php";
+
+if (isset($_POST['masuk'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM user WHERE username = '$username'";
+    $result = $db->query($sql);
+
+    if($result->num_rows > 0 ){
+        $data = $result->fetch_assoc();
+
+        if (password_verify($password, $data['password'])) {
+            header("Location: dashboard.php");
+            exit;
+        } else {
+            echo "Password salah!";
+        }
+    } else {
+        echo "Username tidak ditemukan!";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN PAGE</title>
 </head>
 <body>
@@ -10,29 +37,10 @@
     <?php include "layout/header.html" ?>
     <h2>Login</h2>
     <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="username"> <br> <br>
-        <input type="password" name="password" placeholder="password"> <br> <br>
+        <input type="text" name="username" placeholder="username" required> <br> <br>
+        <input type="password" name="password" placeholder="password" required> <br> <br>
         <button type ="submit" name="masuk">Masuk</button>
     </form>
+
 </body>
 </html>
-
-<?php 
-
-    include "service/user.php";
-
-    if (isset($_POST['masuk'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
-        $result = $db->query($sql);
-
-        if($result->num_rows > 0 ){
-            $data = $result->fetch_assoc();
-
-            header("Location: dashboard.php");
-        }else echo "akun tidak ditemukan";
-    }
-
-?>
